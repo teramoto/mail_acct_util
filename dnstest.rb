e5db86beaa2d3327b9650e2cf883236813e0c827
ken <ken@ray.co.jp>
2015-01-27 21:46:57 +0900

    first commkt on 2015

--- dnstest.rb
+++ dnstest.rb
@@ -2,12 +2,22 @@
 
 require 'resolv' 
 
-hosts = [ "www.ray.co.jp", "www.yahoo.co.jp", "www.apple.com", "wsss.ray.co.jp" ]
+hosts = [ "www.ray.co.jp", "www.yahoo.co.jp", "www.apple.com", "unexisthost.ray.co.jp" ]
 dns = [ "ns.ray.co.jp", "ns1.ray.co.jp", "ns2.ray.co.jp", "ns3.ray.co.jp" ] 
 dns2 = [ "202.213.241.227" , "202.213.241.225" ] 
+dns3 = [ "210.150.40.2", "192.168.212.2", "192.168.200.200" ] 
+class ResolvX < Resolv::DNS 
+  def get_config_info() 
+    return @config_info
+  end
+  def get_ns
+    p @config_info 
+    return @config_info # (:nameserver)
+  end 
+end 
 
 def resolv( hostname, dns)
-  print "looking up #{hostname}@#{dns}.. " 
+  print "looking up #{hostname}@#{dns.get_ns}.. " 
   res = "" 
   dns.each_address( hostname) { | n| 
     puts n
@@ -19,14 +29,26 @@ def resolv( hostname, dns)
   return res 
 end
 $ns = Array.new 
-p = 0 
-dns2.each do |ns|
-  nn = Resolv::DNS.new(:nameserver => ns ) 
-  if nn != nil then 
-    $ns.push(nn)
-  end
+p = 0
+def check_dns_group(dns)  
+  dns.each do |ns|
+#    nn = Resolv::DNS.new(:nameserver => ns ) 
+    nn = ResolvX.new(:nameserver => ns ) 
+    if nn != nil then 
+      nn.timeouts = 5 
+      puts ns 
+      nn.get_config_info
+
+   # _info 
+      $ns.push(nn)
+      p nn 
+#      p nn.getconf_info()
+    end
+  end 
 end 
 
+check_dns_group(dns2)
+check_dns_group(dns3)
 $ns.each do |d|
   hosts.each do |hs|
     resolv( hs, d)
